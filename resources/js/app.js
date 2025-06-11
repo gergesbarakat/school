e3ee551c4ba14ac032b564e88bd91d2fb2e6603b
gerges barakat <girgisbarakat70@gmail.com>
2025-06-11 15:42:51 +0300

    hjukhjkh

--- resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
-
-import DataTable from 'datatables.net-dt';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
--- resources/views/errors/419.blade.php
+++
@@ -1,3 +0,0 @@
-<script>
-    window.location.replace('school/login')
-</script>
